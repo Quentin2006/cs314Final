bdab9207498f9ce6264116357d48adfcbbb21137
Quentin2006 <124484154+Quentin2006@users.noreply.github.com>
2026-02-12 11:37:39 -0800

    fishied messages controller

--- backend/src/controllers/messages.js
+++ backend/src/controllers/messages.js
@@ -1,2 +1,24 @@
-export function getAllMessages(req, res) {
+import Message from '../models/Message.js';
+
+export async function getAllMessages(_, res) {
+  try {
+    const { id } = req.body;
+
+    if (!id) {
+      res.status(400).json({ message: "Contact ID is required" });
+      return;
+    }
+
+    const messages = await Message.find({
+      $or: [
+        { sender: id },
+        { recipient: id }
+      ]
+    }).sort({ createdAt: -1 });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.error("Error in getAllMessages controller: ", error.message);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
 }
